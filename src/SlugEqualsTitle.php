b45a2cc4a6c559deca5d377adc8ab8323e7ddaa0
frederic.koeberl <frederic.koeberl@dynatrace.com>
2023-07-20 10:14:11 +0200

    Fixed bug that occurred when title was empty

--- src/SlugEqualsTitle.php
+++ src/SlugEqualsTitle.php
@@ -42,7 +42,6 @@ class SlugEqualsTitle extends Plugin
                 $toOverwrite = Craft::$app->request->getBodyParam(self::$inputName, "") === "1";
             }
             if (!$toOverwrite) return;
-            
             if (!$element->title) return;
 
             $element->slug = ElementHelper::generateSlug($element->title, null, $element->site->language);
